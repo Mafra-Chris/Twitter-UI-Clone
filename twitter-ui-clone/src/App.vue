bdf6446c1803742376b8c67849575d6c681d3f74
Christiano <mafradesouza.cs@gmail.com>
2020-10-02 22:46:15 -0300

    init

---
+++ twitter-ui-clone/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>Twitter</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
